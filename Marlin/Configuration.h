5d5968ee756fb9f4feb8ab0c4ccde2d4c3b17834
Djordje Mijailovic <jokam85@gmail.com>
2020-01-26 21:23:10 +0100

    Update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -687,7 +687,7 @@
  *      O-- FRONT --+
  *    (0,0)
  */
-#define X_PROBE_OFFSET_FROM_EXTRUDER -50  // X offset: -left  +right  [of the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER -35  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -30  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 0 // Z offset: -below +above  [the nozzle]
 
